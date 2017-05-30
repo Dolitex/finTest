0af23bff9cfc3943c1d944ca254f368ba0d99f8d
Rian F Reis <contato@rianreis.com.br>
2017-05-30 17:37:49 -0300

    adição

---
+++ final.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	char comando;
+	int n1, n2, result;
+
+	printf("Teste final de calc.\n");
+
+	scanf("%c", &comando);
+
+	if(comando == '+')
+	{
+		scanf("%d %d", &n1, &n2);
+
+		result = n1 + n2;
+
+		printf("%d", result);
+		scanf("");
+	}
+
+
+	return 0;
+}
